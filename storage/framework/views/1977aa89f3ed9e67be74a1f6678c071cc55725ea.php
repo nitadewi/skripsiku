9351b7355b742e4d90fafd010af7a3db74ead842
nita dewi <nitadewis@gmail.com>
2020-06-09 20:36:59 +0700

    semoga aman

--- storage/framework/views/1977aa89f3ed9e67be74a1f6678c071cc55725ea.php
+++ storage/framework/views/1977aa89f3ed9e67be74a1f6678c071cc55725ea.php
@@ -83,4 +83,4 @@ endif; ?>
 </div>
 <?php $__env->stopSection(); ?>
 
-<?php echo $__env->make('layouts.main', \Illuminate\Support\Arr::except(get_defined_vars(), ['__data', '__path']))->render(); ?><?php /**PATH C:\xampp\htdocs\bismillah\resources\views/auth/register.blade.php ENDPATH**/ ?>
+<?php echo $__env->make('layouts.app', \Illuminate\Support\Arr::except(get_defined_vars(), ['__data', '__path']))->render(); ?><?php /**PATH C:\xampp\htdocs\bismillah\resources\views/auth/register.blade.php ENDPATH**/ ?>
